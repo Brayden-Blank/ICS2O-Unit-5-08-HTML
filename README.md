5bcef588636ab8703f69318c7fc3a44cd437ed99
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-06-05 22:37:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS2O-Unit-5-08-HTML
+
+[![GitHub's Super Linter](https://github.com/Brayden-Blank/ICS2O-Unit-5-08-HTML/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Brayden-Blank/ICS2O-Unit-5-08-HTML/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Brayden-Blank/ICS2O-Unit-5-08-HTML)](https://repl.it/github/Brayden-Blank/ICS2O-Unit-5-08-HTML)
+
+This site can be found at: [https://Brayden-Blank.github.io/ICS2O-Unit-5-08-HTML](https://Brayden-Blank.github.io/ICS2O-Unit-5-08-HTML)
---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Brayden Blank
+// ICS2O-Unit-5-08-HTML
+// May 2022
+
+'use strict'
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-5-08-HTML/sw.js", {
+    scope: "/ICS2O-Unit-5-08-HTML/",
+  })
+}
+
+/**
+ * Divides the first integer by the second integer.
+ */
+function onButtonClick() {
+  const originalIntegerOne = parseInt(document.getElementById("integerOne").value)
+  const originalIntegerTwo = parseInt(document.getElementById("integerTwo").value)
+  var integerTwo = parseInt(document.getElementById("integerTwo").value)
+  var integerOne
+  let count
+  var answer
+
+  if (originalIntegerOne < 0) {
+    integerOne = 0
+  } else {
+    integerOne = originalIntegerOne
+  }
+  if (integerOne != 0 && integerTwo > 0) {
+    for (count = 0; integerOne >= integerTwo; count++) {
+      // Calculation for + integers divided by + integers
+      integerOne = integerOne - integerTwo
+    }
+    answer = count + "r" + integerOne
+    document.getElementById("answer").innerHTML = "<h5>The result of " + originalIntegerOne + " ÷ " + originalIntegerTwo + " is " + answer + "</h5>"
+  } else if (integerOne != 0 && integerTwo < 0) {
+    for (count = 0; integerOne >= -integerTwo; count++) {
+      // Calculation for + integers divided by - integers
+      integerOne = integerOne - -integerTwo
+    }
+    answer = -count + " with a remainder of " + -integerOne
+    document.getElementById("answer").innerHTML = "<h5>The result of " + originalIntegerOne + " ÷ " + originalIntegerTwo + " is " + answer + "</h5>"
+  } else {
+    document.getElementById("answer").innerHTML = "<h5>The result of " + originalIntegerOne + " ÷ " + originalIntegerTwo + " is undefined</h5>"
+  }
+}
